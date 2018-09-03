4ab7218e36bbbe8fed8a559caf626c97ad43fbda
Dominic Schebeck <ic17b049@technikum-wien.at>
2018-09-04 00:02:07 +0200

    add myUART

--- example_standalone_driverlib2/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/Dominic/git/driverlib_public/driverlib/ccs/Debug/driverlib.lib"
-
